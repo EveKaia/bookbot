b72dcde6200c658d8b63c54bde315d1c603fc3a4
Jessie Grigsby <kaiablackthorne@gmail.com>
2023-12-21 22:13:42 -0600

    Add sorted list and character report

--- main.py
+++ main.py
@@ -3,12 +3,23 @@ def main():
   text = get_book(book_path)
   num_words = count_words(text)
   char_dict = get_chars(text)
-  # print(f"{num_words} words were found in the document")
-  print(char_dict)
+  char_sorted = get_sorted_list(char_dict)
+
+  print(f"--- Begin report of {book_path} ---")
+  print(f"{num_words} words were found in the document\n")
+
+  for item in char_sorted:
+    if not item["char"].isalpha():
+      continue
+    print(f"The {item['char']} character was found {item['num']} times")
+  
   
 def count_words(text):
   words = text.split()
   return len(words)
+
+def sort_key(c):
+  return c["num"]
 
 def get_chars(text):
   # returns dictionary of letters and their count
@@ -23,6 +34,13 @@ def get_chars(text):
     else:
       chars[char_lower] = 1
   return chars
+
+def get_sorted_list(char_dict):
+  sorted_list = []
+  for char in char_dict:
+    sorted_list.append({"char": char, "num": char_dict[char]})
+  sorted_list.sort(reverse=True, key=sort_key)
+  return sorted_list
 
 def get_book(path):
   with open(path) as file:
