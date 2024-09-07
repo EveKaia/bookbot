5f79dcfca04863ce5330b4e2adde6871438baa8c
evekaia <kaiablackthorne@gmail.com>
2024-09-07 00:01:32 -0400

    Add printing of letter counts

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_count = get_word_count(text)
+    letter_dict =  get_letters(text)
     print(f"{word_count} words found in the document")
+    print(letter_dict)
 
 def get_book_text(path):
     with open(path) as f:
@@ -11,5 +13,15 @@ def get_book_text(path):
 def get_word_count(text):
     words = text.split()
     return len(words)
+
+def get_letters(text):
+    letters = {}
+    for letter in text:
+        lowercase = letter.lower()
+        if lowercase in letters:
+            letters[lowercase] += 1
+        else:
+            letters[lowercase] = 1
+    return letters
 
 main()
